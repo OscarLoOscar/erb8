a50046261c670c27dd0a1df1cb557326641b7240
oscarLo <lokongkitoscar@gmail.com>
2025-12-31 15:18:03 +0800

    finish search function

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from listings.models import Listing
 from django.core.paginator import Paginator
+from listings.choices import district_choices,bedroom_choices,room_type_choices
 # Create your views here.
 # 3.最終分黎呢度，2個endpoint
 def listings(request):
@@ -20,4 +21,38 @@ def listing(request,listing_id):
   return render(request,'listings/listing.html',content)
 
 def search(request):
-  return render(request,'listings/search.html')
+  # get all data
+  queryset_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+  # filter keywords
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(title__icontains=keywords)
+  
+  # filter district
+  if 'district' in request.GET:
+    district = request.GET['district']
+    if district:
+      queryset_list = queryset_list.filter(district__iexact=district)
+
+  # filter room
+  if 'rooms' in request.GET:
+    rooms = request.GET['rooms']
+    if rooms:
+      queryset_list = queryset_list.filter(rooms=rooms)
+
+  # filter roomtype
+  if 'room_type' in request.GET:
+    room_type = request.GET['room_type']
+    if room_type:
+      queryset_list = queryset_list.filter(room_type__iexact=room_type)
+
+  context = {
+        'district_choices': district_choices,
+        'bedroom_choices': bedroom_choices,
+        'room_type_choices': room_type_choices,
+        'listings':queryset_list,
+        'values':request.GET
+    }
+  return render(request,'listings/search.html',context)
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 from listings.models import Listing
 from doctors.models import Doctor
+from listings.choices import district_choices,bedroom_choices,room_type_choices
 
 # Create your views here.
 # 3.最終分黎呢度，2個endpoint
@@ -11,8 +12,12 @@ from doctors.models import Doctor
 # pass 個dictionary入黎:
 def index(request):
   # listings = Listing.objects.all() # 大階Listing，拎data model，all() -> 同DB溝通，拎晒所有data
-  listings = Listing.objects.order_by('list_date').filter(is_published=True)[:3] # [:3] -> list -> 0,1,2
-  context = {"listings" : listings}
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3] # [:3] -> list -> 0,1,2
+  context = {'listings' : listings,
+    'district_choices': district_choices,
+    'bedroom_choices':bedroom_choices,
+    'room_type_choices':room_type_choices
+}
   # return render(request,'pages/index.html',{'anything' : 'something','numbers': 1234})
   return render(request,'pages/index.html',context) # 之後會多database，但煩，難改，方便加model
 
@@ -20,7 +25,6 @@ def about(request):
   # return HttpResponse('<h1>about</h1>')
   doctors = Doctor.objects.order_by('-hire_date')[:3]
   mvp_doctors = Doctor.objects.all().filter(is_mvp=True)
-  total_mvp = mvp_doctors.count()
   context = {"doctors":doctors,"mvp_doctors":mvp_doctors}
   return render(request,'pages/about.html',context)
 
